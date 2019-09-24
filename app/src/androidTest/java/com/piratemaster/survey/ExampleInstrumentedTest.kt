5b25d1c325c5dcc8b269433703fe57563c74b64b
piratemaster <slermontov1@gmail.com>
2019-09-24 15:49:29 +0100

    First commit

--- app/src/androidTest/java/com/piratemaster/survey/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/piratemaster/survey/ExampleInstrumentedTest.kt
@@ -1,19 +1,22 @@
+@file:Suppress("DEPRECATION")
+
 package com.piratemaster.survey
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+//import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import androidx.test.runner.AndroidJUnit4 as AndroidJUnit41
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit41::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
